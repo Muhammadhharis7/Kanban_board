61f766b20779972db42b5a81d5c522423d6af2e8
Muhammad1haris <157294037+Muhammad1haris@users.noreply.github.com>
2026-07-28 23:30:58 +0400

    Twenty second commit

--- Kanban_board/Frontend/src/api/axios.js
+++ Kanban_board/Frontend/src/api/axios.js
@@ -1,9 +1,15 @@
 // src/api/axios.js
 import axios from "axios"
 
+// const api = axios.create({
+//     baseURL: `https://kanban-board-29kg.onrender.com/api/v1`,
+//     withCredentials: true   // ← critical: sends cookies with every request
+// })
+
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
-    withCredentials: true   // ← critical: sends cookies with every request
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true
 })
+
 
 export default api
