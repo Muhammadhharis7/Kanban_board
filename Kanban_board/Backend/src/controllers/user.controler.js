e8d23755aa9d1b9c0b39bfa1d2dc0d8cfd20bcc1
Muhammad1haris <157294037+Muhammad1haris@users.noreply.github.com>
2026-07-28 23:35:50 +0400

    Twenty third commit

--- Kanban_board/Backend/src/controllers/user.controler.js
+++ Kanban_board/Backend/src/controllers/user.controler.js
@@ -149,7 +149,8 @@ const logOutUser = asyncHandler(async(req,res)=> {
 
     const options = {
         httpOnly:true,
-        secure:true
+        secure:true,
+        sameSite: "none",
     }
 
     return res
@@ -230,7 +231,8 @@ const refreshAccessToken = asyncHandler(async(req,res) => {
 
         const options = {
             httpOnly:true,
-            secure:true
+            secure:true,
+            sameSite: "none",
         }
 
 
