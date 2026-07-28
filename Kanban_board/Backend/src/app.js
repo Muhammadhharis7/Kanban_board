4140ba9832d9b3471bf2bb1fb20e5582fa9d4fed
Muhammad1haris <157294037+Muhammad1haris@users.noreply.github.com>
2026-07-29 00:09:02 +0400

    fix CORS to allow deployed frontend

--- Kanban_board/Backend/src/app.js
+++ Kanban_board/Backend/src/app.js
@@ -4,10 +4,31 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 // Middleware setup
+// app.use(cors({
+//     origin:process.env.CORS_ORIGIN,
+//     credentials:true
+// }))
+
+
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://kanbanboard-five-flame.vercel.app",
+];
+
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
+
+
 // Allows server to understands the incoming requests with a JSON body
 app.use(express.json({limit:"16kb"})) 
 
